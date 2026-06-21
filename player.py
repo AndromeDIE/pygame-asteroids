1f43914cfbff2717369fa310481a77d02a0b4dc2
AndromeDIE <andreicrismili@gmail.com>
2026-06-21 17:42:54 +0200

    added player with base draw, update and rotation with A and D

---
+++ player.py
@@ -0,0 +1,32 @@
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, LINE_WIDTH
+from circleshape import CircleShape
+import pygame
+
+class Player(CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0
+    
+    def triangle(self) -> list[pygame.Vector2]:
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+    
+    def draw(self, screen: pygame.Surface) -> None:
+        pygame.draw.polygon(screen, "white", self.triangle(), LINE_WIDTH)
+    
+    def rotate(self, dt: float) -> None:
+        self.rotation += dt * PLAYER_TURN_SPEED
+
+    def update(self, dt: float) -> None:
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(dt * -1)
+        
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+
